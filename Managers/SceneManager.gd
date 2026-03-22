32c28375aaeae8bf1a0b88d2abd200aab34ded41
Darthsae <sockmit2007@gmail.com>
2026-03-22 12:54:33 -0400

    Burger.

--- Managers/SceneManager.gd
+++ Managers/SceneManager.gd
@@ -103,7 +103,7 @@ func switch_to(scene_type: Type, init_callback: Callable = Callable()) -> void:
 
 			bingo.wait_to_finish()
 		elif scene_type == Type.EDITOR:
-			CountryManager.clock
+			GameState.current_world.clock.pause()
 		_current_type = scene_type
 
 	if _loading_screen:
