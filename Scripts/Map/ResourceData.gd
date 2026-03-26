9803d92620d7e0d855205da42d001d25060b98d9
Darthsae <sockmit2007@gmail.com>
2026-03-26 00:13:56 -0400

    Caine.

--- Scripts/Map/ResourceData.gd
+++ Scripts/Map/ResourceData.gd
@@ -4,6 +4,7 @@ class_name ResourceData
 @export var name: String
 @export var color: Color
 @export var icon: String
+@export var tags: Dictionary[String, float]
 
 static func FromDict(a_data: Dictionary) -> ResourceData:
 	var resource: ResourceData = ResourceData.new()
@@ -11,15 +12,17 @@ static func FromDict(a_data: Dictionary) -> ResourceData:
 	resource.name = a_data["name"]
 	resource.color = Color.html(a_data.get("color", "#FFFFFF"))
 	resource.icon = a_data.get("icon", "")
+	resource.tags = a_data.get("tags", {})
 	
 	return resource
 
-static func FromValues(a_name: String, a_color: Color, a_icon: String) -> ResourceData:
+static func FromValues(a_name: String, a_color: Color, a_icon: String, a_tags: Dictionary[String, float]) -> ResourceData:
 	var resource: ResourceData = ResourceData.new()
 	
 	resource.name = a_name
 	resource.color = a_color
 	resource.icon = a_icon
+	resource.tags = a_tags
 	
 	return resource
 
@@ -28,4 +31,5 @@ func ToDict() -> Dictionary:
 		"name": self.name,
 		"color": "#" + self.color.to_html(false).to_upper(),
 		"icon": self.icon,
+		"tags": self.tags,
 	}
