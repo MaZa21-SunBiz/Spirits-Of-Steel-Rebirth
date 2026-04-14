dc7a1b1b41a4ef59ff93c934b373d80034446076
Darthsae <sockmit2007@gmail.com>
2026-04-14 08:55:46 -0400

    More work on name generation.

--- Scenes/main.gd
+++ Scenes/main.gd
@@ -3,4 +3,6 @@ class_name Main
 
 
 func _ready() -> void:
+	for i in range(100):
+		print("%3d) %s" % [i + 1, NameGenerator.GenerateName(i % 2 == 0, randi_range(3, 15)).capitalize()])
 	SceneSwitcher.switch_to(SceneSwitcher.Type.MENU)
